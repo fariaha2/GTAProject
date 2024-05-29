3c16e2568a36d7ad0ecf22113ba16aa6225d28e9
Fariah Azam <fariaha124@gmail.com>
2024-05-28 23:21:07 -0400

    welcome panel

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,16 @@
 public class Player {
+    private final double MOVE_AMT = 0.75;
     private double xCoord;
     private double yCoord;
     public Player() {
 
+    }
+
+    public int getxCoord() {
+        return (int) xCoord;
+    }
+
+    public int getyCoord() {
+        return (int) yCoord;
     }
 }
--- src/WelcomePanel.java
+++ src/WelcomePanel.java
@@ -12,36 +12,53 @@ public class WelcomePanel extends JPanel implements ActionListener {
     private JTextField textField;
     private JButton submitButton;
     private JButton clearButton;
+    private JButton tutorialButton;
+    private JButton backButton;
     private JFrame enclosingFrame;
     private BufferedImage background;
 
     public WelcomePanel(JFrame frame) {
-        // image will be added later
         enclosingFrame = frame;
-      //  try {
-            //background = ImageIO.read(new File("src/goomba.png"));
-      //  } catch (IOException e) {
-         //   System.out.println(e.getMessage());
-      //  }
-        textField = new JTextField(10);
+        try {
+            background = ImageIO.read(new File("src/GTALoadingScreen.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        textField = new JTextField(20);
         submitButton = new JButton("Submit");
         clearButton = new JButton("Clear");
+        tutorialButton = new JButton("Tutorial");
+        backButton = new JButton("Back");
         add(textField);
         add(submitButton);
         add(clearButton);
+        add(tutorialButton);
         submitButton.addActionListener(this);
         clearButton.addActionListener(this);
+        tutorialButton.addActionListener(this);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.drawImage(background, 0, 0, null);
+        g.setFont(new Font("Arial", Font.BOLD, 25));
+        g.setColor(Color.WHITE);
+        g.drawString("Welcome to Discount GTA V!", 175, 35);
         g.setFont(new Font("Arial", Font.BOLD, 16));
-        g.setColor(Color.RED);
+        g.setColor(Color.BLACK);
         g.drawString("Please enter your name:", 250, 100);
-        textField.setLocation(280, 120);
-        submitButton.setLocation(280, 140);
-        clearButton.setLocation(350, 140);
+        textField.setLocation(240, 110);
+        submitButton.setLocation(260, 140);
+        clearButton.setLocation(360, 140);
+        tutorialButton.setLocation(300, 300);
+    }
+    public void tutorialGraphics(Graphics g) {
+        super.paintComponent(g);
+        submitButton.setVisible(false);
+        clearButton.setVisible(false);
+        tutorialButton.setVisible(false);
+        textField.setVisible(false);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -51,8 +68,15 @@ public class WelcomePanel extends JPanel implements ActionListener {
                 String playerName = textField.getText();
                 MainFrame f = new MainFrame(playerName);
                 enclosingFrame.setVisible(false);
-            } else {
+            } else if (button == clearButton) {
                 textField.setText("");
+            } else if (button == tutorialButton) {
+                submitButton.setVisible(false);
+                clearButton.setVisible(false);
+                tutorialButton.setVisible(false);
+                textField.setVisible(false);
+            } else {
+
             }
         }
     }
